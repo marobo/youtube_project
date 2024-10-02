6558bfd82c6800e2bf0db6ba35b2d36ab1b32e92
onorio <onorio@catalpa.io>
2024-10-02 11:48:43 +0900

    configuration save file in production works

--- playlist/urls.py
+++ playlist/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from playlist import views
 
@@ -6,4 +7,4 @@ urlpatterns = [
     path('', views.index, name="homepage"),
     path('playlist/', views.youtube_playlist, name='youtube_playlist'),
     path('download/', views.youtube_downloader, name='youtube_downloader'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- playlist/views.py
+++ playlist/views.py
@@ -1,4 +1,5 @@
-import yt_dlp
+import yt_dlp, os, mimetypes
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -29,15 +30,33 @@ def youtube_downloader(request):
 
             else:
                 selected_format_id = request.POST.get('selected_format_id')
+                download_path = os.path.join(settings.MEDIA_ROOT, 'downloaded_files')
+                os.makedirs(download_path, exist_ok=True)
                 ydl_opts = {
                     'format': selected_format_id,
-                    'outtmpl': '~/Downloads/%(title)s.%(ext)s',
+                    'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s'),
                 }
                 try:
                     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                         info_dict = ydl.extract_info(video_url, download=True)
                         video_title = info_dict.get('title', None)
-                        return render(request, 'playlist/youtube_download.html', {'video_title': video_title})
+                        file_path = os.path.join(download_path, f"{video_title}.{info_dict['ext']}")
+
+                        # Serve the file
+                        with open(file_path, 'rb') as f:
+                            response = HttpResponse(f.read())
+                            content_type = mimetypes.guess_type(file_path)[0]
+                            response['Content-Type'] = content_type
+                            response['Content-Disposition'] = f'attachment; filename="{video_title}.{info_dict["ext"]}"'
+
+                            try:
+                                os.remove(file_path)
+                                return render(request, 'playlist/youtube_download.html', {'video_title': video_title})
+
+                            except Exception as e:
+                                print(f"Error deleting file: {e}")
+
+                            return response
 
                 except Exception as e:
                     return HttpResponse(f"Error: {e}")
